2cb94bef92fb77921ffc5808d607f5c5c6d6c379
sacpis@gmail.com <sacpis@gmail.com@c8b68c75-e156-15e8-a0ca-b3660d91254b>
2012-05-03 22:20:36 +0000

    * adding test for checking balance on the MyCardsMain screen.

--- trunk/src/com/sb/test/MyCardsMainTest.java
+++ trunk/src/com/sb/test/MyCardsMainTest.java
@@ -1,11 +1,13 @@
 package com.sb.test;
 
+import static org.junit.Assert.assertEquals;
 import junit.framework.Assert;
 import mockit.NonStrict;
 import mockit.NonStrictExpectations;
 
 import org.junit.Test;
 
+import com.sb.common.CreditBalance;
 import com.sb.controller.AppController;
 import com.sb.controller.ScreenState;
 import com.sb.view.MyCardsMain;
@@ -24,4 +26,13 @@ public class MyCardsMainTest {
         ScreenState returnedScreenState = applet.getCurrentScreen();
         Assert.assertEquals(MyCardsMain.class, returnedScreenState.getClass());
 	}
+    
+    @Test
+    public void testCreditBalanceDisplay() {
+    	double initialBalance = 20.0f;
+    	double actualBalance;
+		CreditBalance bal = CreditBalance.getInstance();
+		actualBalance = CreditBalance.addNewCardbalance();
+		assertEquals(initialBalance, actualBalance, 1f);
+    }
 }
