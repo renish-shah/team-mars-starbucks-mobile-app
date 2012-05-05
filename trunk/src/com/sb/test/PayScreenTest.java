02de281dc2ca7eb5fe4f858db9a89e3a60c258b4
anupama.np@gmail.com <anupama.np@gmail.com@c8b68c75-e156-15e8-a0ca-b3660d91254b>
2012-05-05 01:14:24 +0000

    "Changed a few things in my Junit test case for payonGo "

--- trunk/src/com/sb/test/PayScreenTest.java
+++ trunk/src/com/sb/test/PayScreenTest.java
@@ -5,29 +5,53 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import com.sb.common.CreditBalance;
+import com.sb.view.AddCard;
 import com.sb.view.MyCardsPay;
-
+import junit.framework.Assert;
+import mockit.NonStrict;
+import mockit.NonStrictExpectations;
+import com.sb.controller.AppController;
+import com.sb.controller.ScreenState;
+import com.sb.view.MyCardsPay;
+/**
+  * @author Anupama Patil
+ *
+ */
 public class PayScreenTest {
-
-	@SuppressWarnings("deprecation")
+	
+	@NonStrict AppController applet;
+    @SuppressWarnings("deprecation")
 	@Test
 	public void testSetBalance() {
-		float expectedf = 16.4f;
+		double expectedf = 16.40f;
 		double actualf; 
 		CreditBalance bal = CreditBalance.getInstance();
 		actualf = bal.setBalance();
-		assertEquals("Valid!",expectedf, actualf);
+		Assert.assertEquals("Valid!",expectedf, actualf);
 		
 	}
 	
 	@SuppressWarnings("deprecation")
 	@Test
 	public void testGetBalance() {
-		float expectedf = 20.00f;
+		double expectedf = 20.00f;
 		double actualf; 
 		CreditBalance bal = CreditBalance.getInstance();
 		actualf = bal.getBalance();
-		assertEquals("Valid!",expectedf, actualf);
+		Assert.assertEquals("Valid!",expectedf, actualf);
 		
 	}
-}
+
+      public void MyCardspayTest()
+      {
+          new NonStrictExpectations() {
+            {
+                applet.getCurrentScreen(); returns( new MyCardsPay(applet));
+            }
+        };
+        ScreenState returnedScreenState =  applet.getCurrentScreen();
+        Assert.assertEquals(MyCardsPay.class,returnedScreenState.getClass() );
+    
+      }
+
+}//end of class
